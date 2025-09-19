4c1220cda36ba4ed53d8896021eba6ad117faf12
nicholasthomas18 <nt325@byu.edu>
2025-09-19 13:41:06 -0600

    custom printing

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from utils import print_custom
+
+def main():
+    print("Hello, GitHub!")
+    print_custom("This is a custom message from the print_custom module.")
+
 def main():
     print("Hello from github-issue-page-demo!")
 
